161c6e4409ba4aa5e160e7cc9cedf8fd66382104
ArcaneTAK <arcanetak@gmail.com>
2026-01-15 20:27:19 +0700

    Why does init a repo locally, then reverting change delete all my file??????

---
+++ PopLoader/FileConverter/Rsb/RsbAutoPoolInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace PopLoader.FileConverter.Rsb
+{
+    public class RsbAutoPoolInfo
+    {
+        
+        public string ID;
+        public int part1_MaxOffset_InDecompress;
+        public int part1_MaxSize;
+        public int type = 1;
+
+        public RsbAutoPoolInfo Read(BinaryReader bs)
+        {
+            ID = new string(bs.ReadChars(128));
+            part1_MaxOffset_InDecompress = bs.ReadInt32();
+            part1_MaxSize = bs.ReadInt32();
+            type = bs.ReadInt32();
+            _ = bs.ReadInt32();
+            _ = bs.ReadInt32();
+            _ = bs.ReadInt32();
+            return this;
+        }
+    }
+}
